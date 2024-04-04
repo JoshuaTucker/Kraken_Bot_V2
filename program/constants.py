334311cc3326bc4328a842c5cf0c526a5da152cb
Josh <jtuck917@gmail.com>
2024-04-04 00:50:37 -0400

    second commit

--- program/constants.py
+++ program/constants.py
@@ -1,1 +1,5 @@
-from decouple import 
+from decouple import config
+
+# keys
+API_KEY = config("api_key")
+SEC_KEY = config("private_key")
--- program/func_connections.py
+++ program/func_connections.py
@@ -1,24 +1,29 @@
 import ccxt
-import dontshare_config
-
-# import #your own functions from a file
+from decouple import config
 from datetime import date, datetime, timezone, tzinfo
 from pprint import pprint
+from constants import (
+    API_KEY,
+    SEC_KEY
+)
+
+
+
 
 kraken = ccxt.kraken({
     'enableRateLimit': True,
-    'apiKey': dontshare_config.xP_KEY,
-    'secret': dontshare_config.xP_SEC,
+    'apiKey': API_KEY,
+    'secret': SEC_KEY,
 })
+pprint(kraken.requiredCredentials)
+def get_bid_ask():
+    sol_order_book = kraken.fetch_order_book('SOL/USD')
+    #print(sol_order_book)
+    sol_bid = sol_order_book['bids'][0][0]
+    sol_ask = sol_order_book['asks'][0][0]
+    print(f'the best bid: {sol_bid} the best ask {sol_ask}')
 
-#def get_bid_ask():
-#    sol_order_book = kraken.fetch_order_book('SOL/USD')
-#    #print(sol_order_book)
-#    sol_bid = sol_order_book['bids'][0][0]
-#    sol_ask = sol_order_book['asks'][0][0]
-#    print(f'the best bid: {sol_bid} the best ask {sol_ask}')
-#
-#    return sol_bid, sol_ask
+    return sol_bid, sol_ask
 
 #makepretty = get_bid_ask()
 #pprint(makepretty)
@@ -37,4 +42,14 @@ kraken = ccxt.kraken({
 #pretty = kraken.fetch_currencies()
 #pprint(pretty)
 
-pprint(kraken.fetch_balance())
+# x = kraken.fetch_balance()
+# 
+#x = kraken.fetch_accounts()
+#print(x)
+credits = { "USD" : 'USD'}
+#bal = kraken.fetch_balance()
+#pprint(bal)
+print("//////////////////////////////","\n")
+print(kraken.fetchBalance())
+pprint(get_bid_ask())
+print("works")
